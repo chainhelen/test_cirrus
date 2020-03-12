9173a63d3be36d02ef41cefd9033dcf6e55e1417
chainhelen <chainhelen@gmail.com>
2020-03-12 15:43:44 +0800

    Update teststepconcurrent.go

--- teststepconcurrent.go
+++ teststepconcurrent.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
 	"time"
+	"sync"
 )
 
 var v int = 99
@@ -13,7 +12,6 @@ var s string
 
 func Foo(x, y int) (z int) {
 	//s = fmt.Sprintf("x=%d, y=%d, z=%d\n", x, y, z)
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 	z = x + y
 	return
 }
@@ -24,7 +22,7 @@ func Threads(fn func(x, y int) int) {
 		go func() {
 			for k := 0; k < 100; k++ {
 				fn(1, 2)
-				time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+				time.Sleep(10 * time.Millisecond)
 			}
 			wg.Done()
 		}()
@@ -32,7 +30,6 @@ func Threads(fn func(x, y int) int) {
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	x := v
 	y := x * x
 	var z int
